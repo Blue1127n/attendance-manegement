bdb83981034a6b5061f0cbabeb3d21316d501036
ao1127 <yohijet271@gmail.com>
2025-03-16 22:08:27 +0900

    ロゴ保存

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'coachtech')</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="header__logo">
+                <img src="{{ asset('images/logo.png') }}" alt="Logo">
+            </div>
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
